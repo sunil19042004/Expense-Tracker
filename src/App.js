4808422d715b6f51e0d4897e66e956e8fa3d9b56
SUNIL K <108710362+sunil19042004@users.noreply.github.com>
2024-02-07 18:55:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// FileName: App.js 
+
+import styled from "styled-components"; 
+import Tracker from "./components/Tracker"; 
+import GlobalStyles from "./globalStyles"; 
+
+const Main = styled.div` 
+display: flex; 
+justify-content: center; 
+align-items: center; 
+`; 
+
+const App = () => { 
+return ( 
+	<Main> 
+	<GlobalStyles/>
+	<Tracker/> 
+	</Main> 
+) 
+} 
+
+export default App;
